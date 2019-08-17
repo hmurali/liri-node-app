39943ed23c748e3338cfb88d864927a7b9736762
Hari <augten95@gmail.com>
2019-08-17 15:52:05 -0500

    Added cases for movie-this and do-what-it-says along with movieThis and simonSays functions

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+var fs = require("fs");
 var keys = require("./keys.js");
 const Spotify = require("node-spotify-api");
 var axios = require("axios");
@@ -27,6 +28,15 @@ switch(command) {
         var songInput = nodeArgs.slice(3).join(" ");
         console.log("song to search for: " + songInput);
         spotifyThisSong(songInput);
+        break;
+    case 'movie-this':
+        console.log("nodeArgs: " + nodeArgs);
+        var movieInput = nodeArgs.slice(3).join(" ");
+        console.log("movie to search for: " + movieInput);
+        movieThis(movieInput);
+        break;
+    case 'do-what-it-says':
+        simonSays();
         break;
 }
 
@@ -75,3 +85,50 @@ function spotifyThisSong(song) {
         console.log("Album: " + songInfo[0].album.name);
     });
 }
+
+function movieThis(movie) {
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=full&apikey=trilogy";
+    console.log("queryURL is: " + queryURL);
+    if(!movie) {
+        movie = "Mr Nobody";
+        queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=full&apikey=trilogy";
+        console.log("Mr. Nobody query url: " + queryURL);
+    }
+    axios.get(queryURL).then(function(response) {
+        console.log("Title of the movie: " + response.data.Title);
+        console.log("Year the movie came out: " + response.data.Year);
+        console.log("IMDB rating of the movie: " + response.data.imdbRating);
+        console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+        console.log("Country where the movie was produced: " + response.data.Country);
+        console.log("Language of the movie: " + response.data.Language);
+        console.log("Plot of the movie: " + response.data.Plot);
+        console.log("Actors in the movie: " + response.data.Actors);
+    })
+    .catch(function(error) {
+        if(error.response) {
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log(error.request);
+        } else {
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
+}
+
+function simonSays(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if(error) {
+            return console.log(error);
+        }
+
+        console.log(data);
+        var dataArr = data.split(",");
+        spotifyThisSong(dataArr[1]);
+    })
+};
